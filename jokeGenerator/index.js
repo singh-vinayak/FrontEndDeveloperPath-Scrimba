4ff981085038a7f3de2128ae66bf90d16ad50b6f
Vinayak Singh <vinayaksingh5302@gmail.com>
2021-03-22 17:54:35 +0530

    added punchline

--- jokeGenerator/index.js
+++ jokeGenerator/index.js
@@ -4,6 +4,22 @@ const setupDiv = document.getElementById("setup");
 const punchlineDiv = document.getElementById("punchline");
 const punchlineBtn = document.getElementById("punchlineBtn");
 const newJokeBtn = document.getElementById("newJokeBtn");
+let punchline;
+
+// Add an event listener for the punchline button. When clicked it should call a function called getPunchline
+/* Create the getPunchline function. This function should: 
+    Insert the punchline into the punchlineDiv
+    Add the class "bubble" to the punchlineDiv
+    Toggle the "hidden" class on both buttons.
+*/
+punchlineBtn.addEventListener('click', getPunchline);
+
+function getPunchline() {
+    punchlineDiv.innerHTML = punchline;
+    punchlineDiv.classList.add('bubble');
+    punchlineBtn.classList.toggle('hidden');
+    newJokeBtn.classList.toggle('hidden');
+}
 
 // Setup an async function called getJoke
 // Create a variable called jokePromise that fetches a joke from https://official-joke-api.appspot.com/jokes/programming/random
@@ -13,7 +29,15 @@ async function getJoke() {
     const jokePromise = await fetch('https://official-joke-api.appspot.com/jokes/programming/random');
     const joke = await jokePromise.json();
 
-    console.log(joke);
+    // Get the setup from the joke and insert it into the setupDiv element
+    setupDiv.innerHTML = joke[0].setup;
+
+    // Create a global variable called punchline which will store the current punchline and will be updated with each new joke
+    // Assign the current jokes punchline to the punchline variable.
+    punchline = joke[0].punchline;
+
+    punchlineBtn.classList.toggle('hidden');
+    newJokeBtn.classList.toggle('hidden');
 }
 
 getJoke();
