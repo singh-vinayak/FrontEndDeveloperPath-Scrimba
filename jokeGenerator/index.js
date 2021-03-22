16c414e7051f712b7104b9f728a14f10d7513f41
Vinayak Singh <vinayaksingh5302@gmail.com>
2021-03-22 17:39:14 +0530

    added js file

---
+++ jokeGenerator/index.js
@@ -0,0 +1,19 @@
+// Create 4 variables and assign them to their respective elements: setupDiv, punchlinDiv, punchlineBtn, newJokeBtn
+
+const setupDiv = document.getElementById("setup");
+const punchlineDiv = document.getElementById("punchline");
+const punchlineBtn = document.getElementById("punchlineBtn");
+const newJokeBtn = document.getElementById("newJokeBtn");
+
+// Setup an async function called getJoke
+// Create a variable called jokePromise that fetches a joke from https://official-joke-api.appspot.com/jokes/programming/random
+// create a variable called joke that consumes the json data
+
+async function getJoke() {
+    const jokePromise = await fetch('https://official-joke-api.appspot.com/jokes/programming/random');
+    const joke = await jokePromise.json();
+
+    console.log(joke);
+}
+
+getJoke();
